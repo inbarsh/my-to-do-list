c411e01e5edc3d307aa0f393bc5e70a435337017
inbarsh <inbar.xo@gmail.com>
2020-03-10 15:27:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1 class="app-title">
+    Welcome to {{ title }}!
+  </h1>
+
+  <app-list-manager></app-list-manager>
+`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'My To Do List APP';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
+}
